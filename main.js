4d3bb04b16f4fd906d15429a59a881c11b22c2da
jiannejose <annejizrelle@gmail.com>
2017-12-14 02:40:59 +0800

    calculator initial commit js

---
+++ main.js
@@ -0,0 +1,66 @@
+/******************************************
+ * show operations on solution field 
+
+
+var showOperations = document.getElementsByClassName('operation');
+
+for (var i = 0; i < showOperations.length; i++) {
+    showOperations[i].addEventListener('click', operate);
+}
+
+function operate() {
+    document.getElementById('solution').innerHTML = this.getAttribute('data-operation');
+};
+ * ***************************************/
+
+/******************************************
+ * show numbers on solution field 
+
+
+var showNumbers = document.getElementsByClassName('number');
+
+for (var num = 0; num < showNumbers.length; num++) {
+    showNumbers[num].addEventListener('click', showNum);
+}
+
+function showNum() {
+    document.getElementById('solution').innerHTML += this.getAttribute('data-number');
+}
+ * ***************************************/
+
+/**************** */
+
+
+var given1String = "";
+var given2String = "";
+var chosenOperation = "";
+var numbers = document.getElementsByClassName('number');
+var operations = document.getElementsByClassName('operation');
+
+/* given numbers */
+function showGiven() {
+
+    if (chosenOperation == "") {
+        given1String += this.getAttribute('data-number');
+        document.getElementById('solution').innerHTML = given1String;
+    } else {
+        given2String += this.getAttribute('data-number');
+        document.getElementById('solution').innerHTML = given1String + chosenOperation + given2String; 
+    }
+}
+
+for (i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', showGiven);
+}
+
+
+/* operation */
+
+function usedOperation() {
+    chosenOperation = this.getAttribute('data-operation');
+    document.getElementById('solution').innerHTML = (given1String + chosenOperation);
+}
+
+for (j = 0; j < operations.length; j++) {
+    operations[j].addEventListener('click', usedOperation);
+}
