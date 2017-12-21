b9f74c4a5a8151a3de3786e6df0362d5f9183133
jiannejose <annejizrelle@gmail.com>
2017-12-22 00:45:08 +0800

    Calc 2.0 - show operation

--- calculator-v.2.0/main.js
+++ calculator-v.2.0/main.js
@@ -1,11 +1,32 @@
+/* showing numbers */
 var given = "";
 var numbers = document.getElementsByClassName('number');
 
-function showGiven() {
-    given += this.getAttribute('data-number');
-    document.getElementById('answer').innerHTML = given;
+function showNum() {
+
+    if (usedOperation == "") {
+        given += this.getAttribute('data-number');
+        document.getElementById('answer').innerHTML = given;
+    } else if (usedOperation != "") {
+        document.getElementById('solution').innerHTML = given + usedOperation;
+    }
+
 }
 
 for (i = 0; i < numbers.length; i++ ) {
-    numbers[i].addEventListener('click', showGiven);
+    numbers[i].addEventListener('click', showNum);
+}
+
+/* showing operation */
+var usedOperation = "";
+var operations = document.getElementsByClassName('operation');
+
+function operationUsed() {
+    usedOperation = this.getAttribute('data-operation');
+    document.getElementById('solution').innerHTML = given + " " + usedOperation + " ";
+
+}
+
+for (j = 0; j < operations.length; j++) {
+    operations[j].addEventListener('click', operationUsed);
 }
